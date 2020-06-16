04037c8530d5b81d0437cceff769583f569a0add
Brenek Harrison <brenekharrison@gmail.com>
2020-06-16 14:05:30 -0600

    Remove random numbers

--- main.py
+++ main.py
@@ -16,23 +16,8 @@ app.config['DEBUG'] = False
 #turn the flask app into a socketio app
 socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
 
-#random number Generator Thread
-thread = Thread()
-thread_stop_event = Event()
-no_numbers = False
-
 redcedar_comm_queue = Queue()
 redcedar_obj = None
-
-def randomNumberGenerator():
-	"""
-	Generate a random number every 1 second and emit to a socketio instance (broadcast)
-	"""
-	while not thread_stop_event.isSet():
-		number = round(random()*10, 3)
-		print(number)
-		socketio.emit('newnumber', {'number': number}, namespace='/websocket')
-		socketio.sleep(5)
 
 def run_redcedar():
 	global redcedar_obj
@@ -52,14 +37,7 @@ def index():
 @socketio.on('connect', namespace='/websocket')
 def test_connect():
 	redcedar_comm_queue.put("new connection")
-	# need visibility of the global thread object
-	global thread
 	print('Client connected')
-
-	#Start the random number generator thread only if the thread has not been started before.
-	if not no_numbers and not thread.isAlive():
-		print("Starting Thread")
-		thread = socketio.start_background_task(randomNumberGenerator)
 
 @socketio.on('disconnect', namespace='/websocket')
 def test_disconnect():
@@ -74,8 +52,7 @@ if __name__ == '__main__':
 	else:
 		print("Starting redcedar")
 		socketio.start_background_task(run_redcedar)
-	if "nonumbers" in argv:
-		no_numbers = True
+	
 	socketio.run(app, host="0.0.0.0")
 	
 	if redcedar_obj != None:
