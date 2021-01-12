9bb3cd947ff59fcbc74929db53626ee917db107f
Brenek Harrison <brenekharrison@gmail.com>
2021-01-12 16:43:11 -0700

    Playing with path/filepath

--- controller/main.go
+++ controller/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/BrenekH/project-redcedar-controller/server"
+import (
+	"os"
+)
+
+// IsDirectory returns a bool representing whether or not the provided path is a directory
+func IsDirectory(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return fileInfo.IsDir(), err
+}
 
 func main() {
 	// windowsMediaInfo := "MediaInfo.exe"
@@ -19,5 +30,29 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 	// fmt.Println(string(info))
-	server.RunHTTPServer()
+
+	// paths, err := filepath.Glob("I:/redcedar_test_env/**/*")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// p, err := filepath.Glob("I:/redcedar_test_env/*")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// paths = append(paths, p...)
+
+	// filteredPaths := make([]string, 0)
+
+	// for _, path := range paths {
+	// 	if b, _ := IsDirectory(path); !b {
+	// 		fmt.Println(path)
+	// 		filteredPaths = append(filteredPaths, path)
+	// 	}
+	// }
+	// fmt.Println(filteredPaths)
+
+	// fmt.Println(filepath.ToSlash(filepath.Clean("I:/redcedar_test_env/")) + "/**/*")
+	// fmt.Println(filepath.ToSlash(filepath.Clean("I:/redcedar_test_env")) + "/*")
 }
