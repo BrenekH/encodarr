2dc2d8652d9c4487742ff3ddca8fa8890e6a5c62
Brenek Harrison <brenekharrison@gmail.com>
2021-03-09 11:55:50 -0700

    Initiate SQLite Client

---
+++ controller/db/db.go
@@ -0,0 +1,66 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/BrenekH/logange"
+	"github.com/BrenekH/project-redcedar-controller/options"
+
+	// Load SQLite driver
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Client is a "database/sql" DB pointer for access to the SQLite database
+var Client *sql.DB
+
+var logger logange.Logger
+
+var schemaStmt string = `
+CREATE TABLE IF NOT EXISTS libraries (
+	ID integer,
+	folder text,
+	fs_check_interval integer,
+	pipeline binary,
+	queue binary,
+	file_cache binary
+);
+
+CREATE TABLE IF NOT EXISTS files (
+	filename text,
+	modtime timestamp
+);
+
+CREATE TABLE IF NOT EXISTS history (
+	time_completed timestamp,
+	filename text,
+	messages binary
+);
+
+CREATE TABLE IF NOT EXISTS dispatched_jobs (
+	job binary,
+	status binary,
+	runner text,
+	last_updated timestamp
+);
+`
+
+func init() {
+	// Setup logger
+	logger = logange.NewLogger("database")
+
+	// Setup SQLite database
+	var err error
+	Client, err = sql.Open("sqlite3", options.ConfigDir()+"data.db")
+	if err != nil {
+		logger.Critical(err.Error())
+	}
+
+	var stmt *sql.Stmt
+	stmt, err = Client.Prepare(schemaStmt)
+	if err != nil {
+		logger.Critical(err.Error())
+	}
+	defer stmt.Close()
+	stmt.Exec()
+	logger.Debug("Database setup and ready")
+}
--- controller/go.mod
+++ controller/go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/BrenekH/logange v0.3.0
 	github.com/google/uuid v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.6
 )
