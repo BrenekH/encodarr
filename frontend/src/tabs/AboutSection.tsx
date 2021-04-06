fc9f38e0f23dca4dd0dec6449f48c39b9b482715
Brenek Harrison <brenekharrison@gmail.com>
2021-04-06 17:40:01 -0600

    Update list style and bold titles in about section

--- frontend/src/tabs/AboutSection.tsx
+++ frontend/src/tabs/AboutSection.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React from "react";
 
+import "./AboutSection.css";
+
 interface IAboutSectionState {
 	controller_version: string,
 	web_api_versions: Array<string>,
@@ -43,13 +45,17 @@ export default class AboutSection extends React.Component<{}, IAboutSectionState
 		return (<>
 			<h5>About Encodarr</h5>
 
-			<p>License: This project is licensed under the GNU Public License v2</p>
+			{/* TODO: Mark with proper license */}
+			<p><b>License:</b> The project license is still being decided on</p>
 
-			<p>Controller Version: {this.state.controller_version}</p>
-			<p>Supported Web API Versions: {this.state.web_api_versions.join(", ")}</p>
-			<p>Supported Runner API Versions: {this.state.runner_api_versions.join(", ")}</p>
+			<p><b>Controller Version:</b> {this.state.controller_version}</p>
+			<p className="list-title"><b>Supported API Versions:</b></p>
+			<ul className="api-list">
+				<li><b>Web:</b> {this.state.web_api_versions.join(", ")}</li>
+				<li><b>Runner:</b> {this.state.runner_api_versions.join(", ")}</li>
+			</ul>
 
-			<p>GitHub Repository: <a href="https://github.com/BrenekH/encodarr">https://github.com/BrenekH/encodarr</a></p>
+			<p><b>GitHub Repository:</b> <a href="https://github.com/BrenekH/encodarr">https://github.com/BrenekH/encodarr</a></p>
 		</>);
 	}
 }
