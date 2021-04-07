72e7f0e73dfa14e6acfe33061f6cd85b086c127f
Brenek Harrison <brenekharrison@gmail.com>
2021-04-06 18:46:15 -0600

    Add basic test to Runner

---
+++ runner/encodarr_runner/tests/test_chop_ms.py
@@ -0,0 +1,11 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path.cwd()))
+print(sys.path)
+
+from datetime import timedelta
+from encodarr_runner import chop_ms
+
+def test_chop_ms():
+	assert chop_ms(timedelta(seconds=1_000_000.9)) == timedelta(seconds=1_000_000)
