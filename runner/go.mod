e743c818ec55c29b7e104581e689717661632ef0
Brenek Harrison <brenekharrison@gmail.com>
2021-04-11 16:31:12 -0600

    Add basic structure (TODOs)

--- runner/go.mod
+++ runner/go.mod
@@ -1,3 +1,5 @@
 module github.com/BrenekH/encodarr/runner
 
 go 1.16
+
+require github.com/BrenekH/logange v0.4.0
--- runner/main.go
+++ runner/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"github.com/BrenekH/logange"
+)
+
+var (
+	logger logange.Logger
+)
+
+func init() {
+	logger = logange.NewLogger("main")
+
+	// Setup the root logger to print everything
+	// This really shouldn't stay here
+	f := logange.StandardFormatter{FormatString: "${datetime}|${name}|${lineno}|${levelname}|${message}\n"}
+
+	rootStdoutHandler := logange.NewStdoutHandler()
+	rootStdoutHandler.SetFormatter(f)
+	rootStdoutHandler.SetLevel(logange.LevelTrace)
+
+	logange.RootLogger.AddHandler(&rootStdoutHandler)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	logger.Info("Hello, World!")
+}
+
+// Run runs the basic loop of the Runner
+func Run() {
+	for {
+		// TODO: Send new job request
+		// TODO: Start job with request info
+		for { // TODO: Add stop condition (job is no longer running)
+			// TODO: Get status from job
+			// TODO: Send status to Controller
+		}
+		// TODO: Send job complete
+	}
 }
