b911dcfc3d0a0edfed7435757ee44e60f2319798
Brenek Harrison <brenekharrison@gmail.com>
2021-04-12 14:22:29 -0600

    Basic parsing. Need to decide on the status format

--- runner/cmd_runner/cmd_runner.go
+++ runner/cmd_runner/cmd_runner.go
@@ -3,12 +3,16 @@ package cmd_runner
 import (
 	"fmt"
 	"os/exec"
+	"regexp"
+	"strconv"
 )
 
 type CmdRunner struct {
 	Executable string
 	BaseArgs   []string
 	done       bool
+	fps        float64
+	speed      float64
 }
 
 func (r *CmdRunner) Done() bool {
@@ -22,15 +26,48 @@ func (r *CmdRunner) Start(args []string) {
 	errPipe, _ := c.StderrPipe()
 	b := make([]byte, 1024)
 
+	fpsRe, err := regexp.Compile(`fps= *([0-9\.]*)`)
+	if err != nil {
+		panic(err)
+	}
+
+	timeRe, err := regexp.Compile(`time= *([0-9:\.]*)`)
+	if err != nil {
+		panic(err)
+	}
+
+	speedRe, err := regexp.Compile(`speed= *([0-9\.]*)`)
+	if err != nil {
+		panic(err)
+	}
+
 	go func() {
 		fmt.Println("Starting FFmpeg command")
 		c.Start()
 
 		for {
 			n, err := errPipe.Read(b)
+			line := string(b[:n])
 			fmt.Println(err, n)
-			fmt.Println(string(b[:n]))
-			// TODO: Parse out status from line
+			fmt.Println(line)
+
+			// TODO: Move parsing to function
+			// FPS
+			r.fps, err = strconv.ParseFloat(fpsRe.FindStringSubmatch(line)[1], 64)
+			if err != nil {
+				panic(err)
+			}
+
+			// Time
+			timeStr := timeRe.FindStringSubmatch(line)[1]
+
+			// Speed
+			r.speed, err = strconv.ParseFloat(speedRe.FindStringSubmatch(line)[1], 64)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(r.fps, timeStr, r.speed)
 
 			if n == 0 {
 				break
