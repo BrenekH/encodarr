82e2acc36b15a3100745775252fae8a60d6d42f5
Brenek Harrison <brenekharrison@gmail.com>
2021-04-12 14:39:34 -0600

    Make parsing the ffmpeg stats its own function

--- runner/cmd_runner/cmd_runner.go
+++ runner/cmd_runner/cmd_runner.go
@@ -7,11 +7,38 @@ import (
 	"strconv"
 )
 
+var (
+	fpsRe   *regexp.Regexp
+	timeRe  *regexp.Regexp
+	speedRe *regexp.Regexp
+)
+
+func init() {
+	// Create Regexes and panic if they fail to compile.
+	// This allows them to be used for every regex search instead of needing to recompile everytime.
+	var err error
+	fpsRe, err = regexp.Compile(`fps= *([0-9\.]*)`)
+	if err != nil {
+		panic(err)
+	}
+
+	timeRe, err = regexp.Compile(`time= *([0-9:\.]*)`)
+	if err != nil {
+		panic(err)
+	}
+
+	speedRe, err = regexp.Compile(`speed= *([0-9\.]*)`)
+	if err != nil {
+		panic(err)
+	}
+}
+
 type CmdRunner struct {
 	Executable string
 	BaseArgs   []string
 	done       bool
 	fps        float64
+	time       string
 	speed      float64
 }
 
@@ -26,21 +53,6 @@ func (r *CmdRunner) Start(args []string) {
 	errPipe, _ := c.StderrPipe()
 	b := make([]byte, 1024)
 
-	fpsRe, err := regexp.Compile(`fps= *([0-9\.]*)`)
-	if err != nil {
-		panic(err)
-	}
-
-	timeRe, err := regexp.Compile(`time= *([0-9:\.]*)`)
-	if err != nil {
-		panic(err)
-	}
-
-	speedRe, err := regexp.Compile(`speed= *([0-9\.]*)`)
-	if err != nil {
-		panic(err)
-	}
-
 	go func() {
 		fmt.Println("Starting FFmpeg command")
 		c.Start()
@@ -51,23 +63,13 @@ func (r *CmdRunner) Start(args []string) {
 			fmt.Println(err, n)
 			fmt.Println(line)
 
-			// TODO: Move parsing to function
-			// FPS
-			r.fps, err = strconv.ParseFloat(fpsRe.FindStringSubmatch(line)[1], 64)
-			if err != nil {
-				panic(err)
-			}
+			fps, time, speed := parseFFmpegLine(line)
 
-			// Time
-			timeStr := timeRe.FindStringSubmatch(line)[1]
+			r.fps = fps
+			r.time = time
+			r.speed = speed
 
-			// Speed
-			r.speed, err = strconv.ParseFloat(speedRe.FindStringSubmatch(line)[1], 64)
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Println(r.fps, timeStr, r.speed)
+			fmt.Println(r.fps, r.time, r.speed)
 
 			if n == 0 {
 				break
@@ -87,4 +89,25 @@ func NewCmdRunner() CmdRunner {
 		BaseArgs:   []string{"-hide_banner", "-loglevel", "warning", "-stats", "-y"},
 		done:       false,
 	}
+}
+
+// parseFFmpegLine parses out the fps, time, and speed information from a standard FFmpeg statistics line.
+// There might be a speed up that involves changing the line parameter(and maybe the return results) to a pointer
+// (avoids copying the value for a new frame), but the jury is still out on that one.
+func parseFFmpegLine(line string) (fps float64, time string, speed float64) {
+	// FPS
+	fps, err := strconv.ParseFloat(fpsRe.FindStringSubmatch(line)[1], 64)
+	if err != nil {
+		panic(err)
+	}
+
+	// Time
+	time = timeRe.FindStringSubmatch(line)[1]
+
+	// Speed
+	speed, err = strconv.ParseFloat(speedRe.FindStringSubmatch(line)[1], 64)
+	if err != nil {
+		panic(err)
+	}
+	return
 }
