c5f8072f0bac231b340396ca686ecf1f4d5192ff
Brenek Harrison <brenekharrison@gmail.com>
2021-04-15 18:09:49 -0600

    Encode and create stereo audio in one step

--- runner/http/http.go
+++ runner/http/http.go
@@ -190,9 +190,10 @@ type history struct {
 
 // genFFmpegCmd creates the correct ffmpeg arguments for the input/output filenames and the job parameters.
 func genFFmpegCmd(inputFname, outputFname string, params jobParameters) []string {
-	// TODO: Encode and add stereo audio track in one step
 	var s []string
-	if params.Stereo {
+	if params.Stereo && params.Encode {
+		s = []string{"-i", inputFname, "-map", "0:v", "-map", "0:s?", "-map", "0:a", "-map", "0:a", "-c:v", params.Codec, "-c:s", "copy", "-c:a:1", "copy", "-c:a:0", "aac", "-filter:a:0", "pan=stereo|FL=0.5*FC+0.707*FL+0.707*BL+0.5*LFE|FR=0.5*FC+0.707*FR+0.707*BR+0.5*LFE", outputFname}
+	} else if params.Stereo {
 		s = []string{"-i", inputFname, "-map", "0:v", "-map", "0:s?", "-map", "0:a", "-map", "0:a", "-c:v", "copy", "-c:s", "copy", "-c:a:1", "copy", "-c:a:0", "aac", "-filter:a:0", "pan=stereo|FL=0.5*FC+0.707*FL+0.707*BL+0.5*LFE|FR=0.5*FC+0.707*FR+0.707*BR+0.5*LFE", outputFname}
 	} else if params.Encode {
 		s = []string{"-i", inputFname, "-map", "0:s?", "-map", "0:a", "-c", "copy", "-map", "0:v", "-vcodec", params.Codec, outputFname}
