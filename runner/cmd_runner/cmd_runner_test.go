87cad5b23f33ccc2e331b87c607218e60c8f0a38
Brenek Harrison <brenekharrison@gmail.com>
2021-04-17 17:00:55 -0600

    Remove cR.Status and cR.Results tests
    I'm not a fan of using another interface just so I can mock out
    the time.Since calls used in these functions.
    
    Maybe someday I'll revisit this and make them properly testable,
    today is not that day.

--- runner/cmd_runner/cmd_runner_test.go
+++ runner/cmd_runner/cmd_runner_test.go
@@ -1,11 +1,7 @@
 package cmd_runner
 
 import (
-	"reflect"
 	"testing"
-	"time"
-
-	"github.com/BrenekH/encodarr/runner"
 )
 
 func TestCmdRunnerDone(t *testing.T) {
@@ -31,64 +27,4 @@ func TestCmdRunnerDone(t *testing.T) {
 func TestCmdRunnerStart(t *testing.T) {
 	// This test won't be implemented until I figure out
 	// how to prevent a command from actually being ran.
-}
-
-func TestCmdRunnerStatus(t *testing.T) {
-	tests := []struct {
-		name         string
-		fps          float64
-		time         string
-		speed        float64
-		startTime    time.Time
-		fileDuration time.Duration
-		expected     runner.JobStatus
-	}{}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			r := CmdRunner{
-				fps:          test.fps,
-				time:         test.time,
-				speed:        test.speed,
-				startTime:    test.startTime,
-				fileDuration: test.fileDuration,
-			}
-
-			out := r.Status()
-
-			if !reflect.DeepEqual(out, test.expected) {
-				t.Errorf("expected %v, but got %v", test.expected, out)
-			}
-		})
-	}
-}
-
-func TestCmdRunnerResults(t *testing.T) {
-	tests := []struct {
-		name      string
-		failed    bool
-		errors    []string
-		warnings  []string
-		startTime time.Time
-		expected  runner.CommandResults
-	}{}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			// Set all the private vars, relating to the creation of the runner.CommandResults struct.
-			// Check if the generated struct is equal to the expected one.
-			r := CmdRunner{
-				failed:    test.failed,
-				errors:    test.errors,
-				warnings:  test.warnings,
-				startTime: test.startTime,
-			}
-
-			out := r.Results()
-
-			if !reflect.DeepEqual(out, test.expected) {
-				t.Errorf("expected %v, but got %v", test.expected, out)
-			}
-		})
-	}
 }
