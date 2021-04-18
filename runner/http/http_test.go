5d7e16ece7e3555432bc8922aaf10f6881502328
Brenek Harrison <brenekharrison@gmail.com>
2021-04-18 17:57:34 -0600

    Test that request body is properly made

--- runner/http/http_test.go
+++ runner/http/http_test.go
@@ -13,25 +13,64 @@ import (
 )
 
 func TestApiV1SendStatus(t *testing.T) {
-	// TODO: Test more than just responding to a 409 status code
-
 	apiV1, err := NewApiV1(options.TempDir(), "", "", "")
 	if err != nil {
 		t.Errorf("Unexpected error creating apiV1: %v", err)
+		return
 	}
 
-	apiV1.httpClient = &mock.HTTPClient{
-		DoResponse: netHTTP.Response{
-			StatusCode: 409,
-			Body:       io.NopCloser(&bytes.Buffer{}),
-		},
-	}
+	t.Run("Proper HTTP Request is Made", func(t *testing.T) {
+		in := runner.JobStatus{
+			Stage:                       "running",
+			Percentage:                  "76",
+			JobElapsedTime:              "1s",
+			FPS:                         "9",
+			StageElapsedTime:            "1s",
+			StageEstimatedTimeRemaining: "1s",
+		}
+		out := `{"uuid":"uuid-4","status":{"stage":"running","percentage":"76","job_elapsed_time":"1s","fps":"9","stage_elapsed_time":"1s","stage_estimated_time_remaining":"1s"}}`
 
-	ctx := context.Background()
-	err = apiV1.SendStatus(&ctx, "uuid-4", runner.JobStatus{})
-	if err == nil {
-		t.Errorf("Expected Unresponsive error: %v", err)
-	} else if err != runner.ErrUnresponsive {
-		t.Errorf("Expected Unresponsive error: %v", err)
-	}
+		c := mock.HTTPClient{
+			DoResponse: netHTTP.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(&bytes.Buffer{}),
+			},
+		}
+
+		apiV1.httpClient = &c
+
+		ctx := context.Background()
+		err = apiV1.SendStatus(&ctx, "uuid-4", in)
+
+		if err != nil {
+			t.Errorf("unexpected error %v", err)
+		}
+
+		b, err := io.ReadAll(c.LastRequest.Body)
+		if err != nil {
+			t.Errorf("unexpected error %v", err)
+		}
+
+		if string(b) != out {
+			t.Errorf("expected %v but got %v", out, string(b))
+		}
+	})
+
+	t.Run("Respond to Unresponsive Status Code", func(t *testing.T) {
+		apiV1.httpClient = &mock.HTTPClient{
+			DoResponse: netHTTP.Response{
+				StatusCode: 409,
+				Body:       io.NopCloser(&bytes.Buffer{}),
+			},
+		}
+
+		ctx := context.Background()
+		err = apiV1.SendStatus(&ctx, "uuid-4", runner.JobStatus{})
+
+		if err == nil {
+			t.Errorf("Expected Unresponsive error: %v", err)
+		} else if err != runner.ErrUnresponsive {
+			t.Errorf("Expected Unresponsive error: %v", err)
+		}
+	})
 }
--- runner/http/mock/http_client.go
+++ runner/http/mock/http_client.go
@@ -3,9 +3,11 @@ package mock
 import netHTTP "net/http"
 
 type HTTPClient struct {
-	DoResponse netHTTP.Response
+	DoResponse  netHTTP.Response
+	LastRequest netHTTP.Request
 }
 
 func (h *HTTPClient) Do(req *netHTTP.Request) (*netHTTP.Response, error) {
+	h.LastRequest = *req
 	return &h.DoResponse, nil
 }
