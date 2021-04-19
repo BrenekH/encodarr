f055b9bf74ecc55029f2cb2925a423d3ec05f9d8
Brenek Harrison <brenekharrison@gmail.com>
2021-04-19 14:44:10 -0600

    Increase test coverage of options package

--- runner/options/options_test.go
+++ runner/options/options_test.go
@@ -27,3 +27,57 @@ func TestStringVarFromEnv(t *testing.T) {
 		}
 	})
 }
+
+func TestConfigDir(t *testing.T) {
+	c := ConfigDir()
+	v := configDir
+
+	if c != v {
+		t.Errorf("expected %v but got %v", v, c)
+	}
+}
+
+func TestTempDir(t *testing.T) {
+	td := TempDir()
+	v := tempDir
+
+	if td != v {
+		t.Errorf("expected %v but got %v", v, td)
+	}
+}
+
+func TestRunnerName(t *testing.T) {
+	r := RunnerName()
+	v := runnerName
+
+	if r != v {
+		t.Errorf("expected %v but got %v", v, r)
+	}
+}
+
+func TestControllerIP(t *testing.T) {
+	cip := ControllerIP()
+	v := controllerIP
+
+	if cip != v {
+		t.Errorf("expected %v but got %v", v, cip)
+	}
+}
+
+func TestControllerPort(t *testing.T) {
+	port := ControllerPort()
+	v := controllerPort
+
+	if port != v {
+		t.Errorf("expected %v but got %v", v, port)
+	}
+}
+
+func TestInTestMode(t *testing.T) {
+	tm := InTestMode()
+	v := inTestMode
+
+	if tm != v {
+		t.Errorf("expected %v but got %v", v, tm)
+	}
+}
