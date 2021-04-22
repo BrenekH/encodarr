9e27d59a10bcb1bdb431f6ff3ef42b51277f6052
Brenek Harrison <brenekharrison@gmail.com>
2021-04-21 20:39:25 -0600

    Test cases for TestStatus

--- runner/cmd_runner/cmd_runner_test.go
+++ runner/cmd_runner/cmd_runner_test.go
@@ -50,14 +50,35 @@ func TestStatus(t *testing.T) {
 		timeNow  time.Time
 		expected runner.JobStatus
 	}{
-		{name: "Nothing Custom Set", timeNow: time.Unix(0, 0).UTC(), expected: runner.JobStatus{
-			Stage:                       "Running FFmpeg",
-			Percentage:                  "NaN",
-			JobElapsedTime:              "2562047h47m16.854775807s",
-			FPS:                         "0",
-			StageElapsedTime:            "2562047h47m16.854775807s",
-			StageEstimatedTimeRemaining: "-2562047h47m16.854775808s",
-		}},
+		{
+			name:    "Nothing Custom Set",
+			timeNow: time.Unix(0, 0).UTC(),
+			expected: runner.JobStatus{
+				Stage:                       "Running FFmpeg",
+				Percentage:                  "NaN",
+				JobElapsedTime:              "2562047h47m16.854775807s",
+				FPS:                         "0",
+				StageElapsedTime:            "2562047h47m16.854775807s",
+				StageEstimatedTimeRemaining: "-2562047h47m16.854775808s",
+			},
+		},
+		{
+			name:         "Everything Custom Set",
+			timeNow:      time.Date(2000, time.January, 1, 0, 20, 0, 0, time.UTC),
+			fileDuration: time.Duration(20) * time.Minute,
+			startTime:    time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC),
+			fps:          9.001,
+			time:         "00:10:00",
+			speed:        1.0,
+			expected: runner.JobStatus{
+				Stage:                       "Running FFmpeg",
+				Percentage:                  "50.00",
+				JobElapsedTime:              "20m0s",
+				FPS:                         "9.001",
+				StageElapsedTime:            "20m0s",
+				StageEstimatedTimeRemaining: "10m0s",
+			},
+		},
 	}
 
 	for _, test := range tests {
