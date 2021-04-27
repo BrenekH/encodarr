37fc6edabb3e720f7fa3e1010749b6c6297d5ad7
Brenek Harrison <brenekharrison@gmail.com>
2021-04-26 22:14:47 -0600

    Documentation

--- controller/run.go
+++ controller/run.go
@@ -2,6 +2,8 @@ package controller
 
 import "context"
 
+// Run is the "top-level" function for running the Encodarr Controller. It calls all of the injected
+// dependencies in order to operate.
 func Run(ctx *context.Context, ds DataStorer, hc HealthChecker, lm LibraryManager, rc RunnerCommunicator, ui UserInterfacer) {
 	for {
 		if IsContextFinished(ctx) {
@@ -15,7 +17,7 @@ func Run(ctx *context.Context, ds DataStorer, hc HealthChecker, lm LibraryManage
 		lm.StartFSChecks(ctx)
 
 		// Loops through the dispatched jobs, and determines if any are unresponsive.
-		hc.RunCheck()
+		hc.RunCheck([]struct{}{})
 
 		// Remove any dispatched jobs that have become unresponsive.
 		ds.RemoveDispatchedJob([]string{})
@@ -24,12 +26,17 @@ func Run(ctx *context.Context, ds DataStorer, hc HealthChecker, lm LibraryManage
 		rc.AddNullUUIDs([]string{})
 
 		// Returns Runner statuses to be saved and communicated to the user.
-		rc.GetRunnerInfo()
+		rc.GetRunnerStatuses()
 
 		// Takes the statuses grabbed from GetRunnerInfo and allows them to be seen by the user.
-		ui.SetRunnerStatuses([]struct{}{})
+		ui.SetJobStatuses([]struct{}{})
 
 		// Takes the new job requests from GetRunnerInfo and sends them to the user for display as waiting Runners.
 		ui.SetWaitingRunners([]string{})
+
+		//? What to do with completed jobs?
+
+		//? Should the CommandDecider (plugins and naive approach(current way)) be a top-level interface?
+		//?   - Passing data from the file system checks up to the Run func, to be passed into CommandDecider.
 	}
 }
