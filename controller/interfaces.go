1f1461c50dc340ba64820fb4eaa759e26b9e92a6
Brenek Harrison <brenekharrison@gmail.com>
2021-05-11 14:43:09 -0600

    Clean out old architecture

--- controller/interfaces.go
+++ controller/interfaces.go
@@ -1,56 +1,33 @@
 package controller
 
-import "context"
-
-// The DataStorer interface describes how a struct wishing to store information
-// such as configuration settings and library/dispatched job data should interact
-// with the Run function.
-type DataStorer interface {
-	// RemoveDispatchedJob removes any dispatched jobs that match the passed UUIDs.
-	RemoveDispatchedJob(UUIDs []string)
-}
+type UUID string
 
 // The HealthChecker interface describes how a struct wishing to decide if a job's
 // last update was long enough ago to mark the Runner doing it as unresponsive
 // should interact with the Run function.
 type HealthChecker interface {
-	// RunCheck loops through the provided slice of dispatched jobs and checks if any have
+	// Run loops through the provided slice of dispatched jobs and checks if any have
 	// surpassed the allowed time between updates.
-	RunCheck([]struct{}) []string
+	Run() (uuidsToNull []UUID)
 }
 
 // The LibraryManager interface describes how a struct wishing to deal with user's
 // libraries should interact with the Run function.
 type LibraryManager interface {
-	// StartFSChecks identifies which libraries are due for an update and then
-	// spawns a goroutine for all that do require one.
-	StartFSChecks(ctx *context.Context)
+	// LibraryQueues returns a slice of structs which represent the state of the queues
+	LibraryQueues() (queues []struct{})
 }
 
 // The RunnerCommunicator interface describes how a struct wishing to communicate
 // with external Runners should interact with the Run function.
 type RunnerCommunicator interface {
-	// AddNullUUIDs takes the provided slice of stringed UUIDs and marks them
+	// NullifyUUIDs takes the provided slice of stringed UUIDs and marks them
 	// so that if a Runner sends a request with a nullified UUID, it gets notified
 	// that it is considered unresponsive and should acquire a new job.
-	AddNullUUIDs(uuids []string)
-
-	// GetRunnerStatuses returns the updated statuses of all active Runners.
-	GetRunnerStatuses()
+	NullifyUUIDs(uuids []UUID)
 }
 
 // The UserInterfacer interface describes how a struct wishing to interact
 // with the user should interact with the Run function.
 type UserInterfacer interface {
-	// GetUserInput returns a struct with information about what the user has requested
-	// since the last time this function was called.
-	GetUserInput()
-
-	// SetJobStatuses takes in a slice with information about each active job's status
-	// so that it can be displayed to the user.
-	SetJobStatuses(statuses []struct{})
-
-	// SetWaitingRunners takes in a slice which directly corresponds to Runners' names
-	// which have requested a job, but not yet received one.
-	SetWaitingRunners(runnerNames []string)
 }
--- controller/run.go
+++ controller/run.go
@@ -4,39 +4,6 @@ import "context"
 
 // Run is the "top-level" function for running the Encodarr Controller. It calls all of the injected
 // dependencies in order to operate.
-func Run(ctx *context.Context, ds DataStorer, hc HealthChecker, lm LibraryManager, rc RunnerCommunicator, ui UserInterfacer) {
-	for {
-		if IsContextFinished(ctx) {
-			break
-		}
+func Run(ctx *context.Context, hc HealthChecker, lm LibraryManager, rc RunnerCommunicator, ui UserInterfacer) {
 
-		// GetUserInput returns any settings that the user has changed. Other input may be available in the future, so plan for that.
-		ui.GetUserInput()
-
-		// Launches a goroutine for libraries that are due for a file system check.
-		lm.StartFSChecks(ctx)
-
-		// Loops through the dispatched jobs, and determines if any are unresponsive.
-		hc.RunCheck([]struct{}{})
-
-		// Remove any dispatched jobs that have become unresponsive.
-		ds.RemoveDispatchedJob([]string{})
-
-		// Adds UUIDs to null from the health check.
-		rc.AddNullUUIDs([]string{})
-
-		// Returns Runner statuses to be saved and communicated to the user.
-		rc.GetRunnerStatuses()
-
-		// Takes the statuses grabbed from GetRunnerInfo and allows them to be seen by the user.
-		ui.SetJobStatuses([]struct{}{})
-
-		// Takes the new job requests from GetRunnerInfo and sends them to the user for display as waiting Runners.
-		ui.SetWaitingRunners([]string{})
-
-		//? What to do with completed jobs?
-
-		//? Should the CommandDecider (plugins and naive approach(current way)) be a top-level interface?
-		//?   - Passing data from the file system checks up to the Run func, to be passed into CommandDecider.
-	}
 }
