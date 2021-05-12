dd512cb4a53a44edcbb972f19053749f295fcd60
Brenek Harrison <brenekharrison@gmail.com>
2021-05-12 14:48:36 -0600

    Refactor main.go to actually run the Controller

--- controller/cmd/EncodarrController/main.go
+++ controller/cmd/EncodarrController/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/BrenekH/encodarr/controller"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ctx, cancel := context.WithCancel(context.Background())
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-signals
+		log.Printf("Received stop signal: %v", sig)
+		// logger.Info(fmt.Sprintf("Received stop signal: %v", sig)) // logange.Logger
+		cancel()
+	}()
+
+	// TODO: Replace mocks with actual implemented structs
+	mockHealthChecker := controller.MockHealthChecker{}
+	mockLibraryManager := controller.MockLibraryManager{}
+	mockRunnerCommunicator := controller.MockRunnerCommunicator{}
+	mockUserInterfacer := controller.MockUserInterfacer{}
+
+	controller.Run(&ctx, &mockHealthChecker, &mockLibraryManager, &mockRunnerCommunicator, &mockUserInterfacer, false)
 }
