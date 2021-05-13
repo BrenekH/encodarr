66912fc63fd070d903687c10eb2c5eac1a53dd07
Brenek Harrison <brenekharrison@gmail.com>
2021-05-13 16:13:48 -0600

    job_health test defintions

--- controller/job_health/job_health_test.go
+++ controller/job_health/job_health_test.go
@@ -2,8 +2,12 @@ package job_health
 
 import "testing"
 
-// TODO: Implement
-
 func TestImplement(t *testing.T) {
 	t.Errorf("test file not implemented")
 }
+
+// TODO: Implement
+// Tests to create
+//   - Run calls time.Since() and SettingsStorer.HealthCheckInterval()
+//   - Run only calls DataStorer.DispatchedJobs() when time.Since is greater than SettingsStorer.HealthCheckInterval()
+//   - Various scenarios around dispatched jobs LastUpdated field being higher or lower than HealthCheckTimeout
