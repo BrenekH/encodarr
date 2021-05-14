9ba9616f4ccfe0b118d730169bc703cae969c1f6
Brenek Harrison <brenekharrison@gmail.com>
2021-05-14 15:13:44 -0600

    Test if time.Since and SS.HealthCheckIntrvl called

--- controller/job_health/job_health_test.go
+++ controller/job_health/job_health_test.go
@@ -2,12 +2,26 @@ package job_health
 
 import "testing"
 
-func TestImplement(t *testing.T) {
-	t.Errorf("test file not implemented")
+// Run calls time.Since() and SettingsStorer.HealthCheckInterval()
+func TestTimeSinceAndSSHealthCheckIntervalCalled(t *testing.T) {
+	ds := mockDataStorer{}
+	ss := mockSettingsStorer{}
+	c := NewChecker(&ds, &ss)
+
+	mNS := mockNowSincer{}
+	c.nowSincer = &mNS
+
+	c.Run()
+
+	if !mNS.sinceCalled {
+		t.Errorf("expected NowSincer.Since() to be called")
+	}
+	if !ss.healthCheckIntCalled {
+		t.Errorf("expected SettingsStorer.HealthCheckInterval() to be called")
+	}
 }
 
 // TODO: Implement
 // Tests to create
-//   - Run calls time.Since() and SettingsStorer.HealthCheckInterval()
 //   - Run only calls DataStorer.DispatchedJobs() when time.Since is greater than SettingsStorer.HealthCheckInterval()
 //   - Various scenarios around dispatched jobs LastUpdated field being higher or lower than HealthCheckTimeout
---
+++ controller/job_health/mock.go
@@ -0,0 +1,50 @@
+package job_health
+
+import (
+	"time"
+
+	"github.com/BrenekH/encodarr/controller"
+)
+
+type mockNowSincer struct {
+	nowResp   time.Time
+	sinceResp time.Duration
+
+	nowCalled   bool
+	sinceCalled bool
+}
+
+func (m *mockNowSincer) Now() time.Time {
+	m.nowCalled = true
+	return m.nowResp
+}
+
+func (m *mockNowSincer) Since(time.Time) time.Duration {
+	m.sinceCalled = true
+	return m.sinceResp
+}
+
+type mockDataStorer struct{}
+
+func (m *mockDataStorer) DispatchedJobs() (djs []controller.DispatchedJob) { return }
+func (m *mockDataStorer) DeleteJob(uuid controller.UUID)                   {}
+
+type mockSettingsStorer struct {
+	healthCheckIntCalled bool
+
+	healthCheckInt uint64
+}
+
+func (m *mockSettingsStorer) HealthCheckInterval() uint64 {
+	m.healthCheckIntCalled = true
+	return m.healthCheckInt
+}
+
+func (m *mockSettingsStorer) Load() (err error)              { return }
+func (m *mockSettingsStorer) Save() (err error)              { return }
+func (m *mockSettingsStorer) Close() (err error)             { return }
+func (m *mockSettingsStorer) SetHealthCheckInterval(uint64)  {}
+func (m *mockSettingsStorer) HealthCheckTimeout() (u uint64) { return }
+func (m *mockSettingsStorer) SetHealthCheckTimeout(uint64)   {}
+func (m *mockSettingsStorer) LogVerbosity() (s string)       { return }
+func (m *mockSettingsStorer) SetLogVerbosity(string)         {}
