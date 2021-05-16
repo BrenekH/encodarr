744a222916752d6b32399913de7a9c6424153545
Brenek Harrison <brenekharrison@gmail.com>
2021-05-16 00:13:09 -0600

    Add field tags to FileMetadata struct and children

--- controller/file_metadata.go
+++ controller/file_metadata.go
@@ -1,36 +1,34 @@
 package controller
 
-// TODO: Add json tags (`json:"name"`) to structs
-
 type FileMetadata struct {
-	General        General
-	VideoTracks    []VideoTrack
-	AudioTracks    []AudioTrack
-	SubtitleTracks []SubtitleTrack
+	General        General         `json:"general"`
+	VideoTracks    []VideoTrack    `json:"video_tracks"`
+	AudioTracks    []AudioTrack    `json:"audio_tracks"`
+	SubtitleTracks []SubtitleTrack `json:"subtitle_tracks"`
 }
 
 // NOTE: Track type determined by "@type" for MediaInfo and "codec_type" for FFProbe
 
 type General struct {
 	// It looks like any non-string field will have to be parsed
-	Duration float32
+	Duration float32 `json:"duration"`
 }
 
 type VideoTrack struct {
-	Index          int    // "StreamOrder" (MI), "index" (FF)
-	Codec          string // Either "AVC", "HEVC", etc.
-	Bitrate        int    // "BitRate" (MI), "bit_rate" (FF)
-	Width          int    // "Width" (MI), "width" (FF)
-	Height         int    // "Height" (MI), "height" (FF)
-	ColorPrimaries string // "colour_primaries" (MI), "color_primaries" (FF) Will be different based on which MetadataReader is being used (FF gives "bt2020" while MI gives "BT.2020")
+	Index          int    `json:"index"`           // "StreamOrder" (MI), "index" (FF)
+	Codec          string `json:"codec"`           // Either "AVC", "HEVC", etc.
+	Bitrate        int    `json:"bitrate"`         // "BitRate" (MI), "bit_rate" (FF)
+	Width          int    `json:"width"`           // "Width" (MI), "width" (FF)
+	Height         int    `json:"height"`          // "Height" (MI), "height" (FF)
+	ColorPrimaries string `json:"color_primaries"` // "colour_primaries" (MI), "color_primaries" (FF) Will be different based on which MetadataReader is being used (FF gives "bt2020" while MI gives "BT.2020")
 }
 
 type AudioTrack struct {
-	Index    int // "StreamOrder" (MI), "index" (FF)
-	Channels int // "Channels" (MI), "channels" (FF)
+	Index    int `json:"index"`    // "StreamOrder" (MI), "index" (FF)
+	Channels int `json:"channels"` // "Channels" (MI), "channels" (FF)
 }
 
 type SubtitleTrack struct {
-	Index    int    // "StreamOrder" (MI), "index" (FF)
-	Language string // "Language" (MI), "tags.language"
+	Index    int    `json:"index"`    // "StreamOrder" (MI), "index" (FF)
+	Language string `json:"language"` // "Language" (MI), "tags.language"
 }
