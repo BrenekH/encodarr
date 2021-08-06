202431f8990a9c563fb584715c856881ec9dd22a
Brenek Harrison <brenekharrison@gmail.com>
2021-08-06 17:54:22 -0600

    Fix golint issues in controller/runnercommunicator

--- controller/runnercommunicator/runner_http_api_v1.go
+++ controller/runnercommunicator/runner_http_api_v1.go
@@ -15,6 +15,7 @@ import (
 	"github.com/google/uuid"
 )
 
+// NewRunnerHTTPApiV1 returns a new RunnerHTTPApiV1.
 func NewRunnerHTTPApiV1(logger controller.Logger, httpServer controller.HTTPServer, ds controller.RunnerCommunicatorDataStorer) RunnerHTTPApiV1 {
 	return RunnerHTTPApiV1{
 		logger:         logger,
@@ -26,6 +27,7 @@ func NewRunnerHTTPApiV1(logger controller.Logger, httpServer controller.HTTPServ
 	}
 }
 
+// RunnerHTTPApiV1 satisfies the controller.RunnerCommunicator interface using an HTTP server.
 type RunnerHTTPApiV1 struct {
 	logger     controller.Logger
 	httpServer controller.HTTPServer
@@ -36,6 +38,7 @@ type RunnerHTTPApiV1 struct {
 	wrQueue        queue
 }
 
+// Start starts the HTTP server. It does not block the thread.
 func (r *RunnerHTTPApiV1) Start(ctx *context.Context, wg *sync.WaitGroup) {
 	r.httpServer.Start(ctx, wg)
 
@@ -45,6 +48,7 @@ func (r *RunnerHTTPApiV1) Start(ctx *context.Context, wg *sync.WaitGroup) {
 	r.httpServer.HandleFunc("/api/runner/v1/job/complete", r.jobComplete)
 }
 
+// CompletedJobs returns any completed jobs as told by the Runners.
 func (r *RunnerHTTPApiV1) CompletedJobs() (j []controller.CompletedJob) {
 	for {
 		select {
@@ -56,6 +60,7 @@ func (r *RunnerHTTPApiV1) CompletedJobs() (j []controller.CompletedJob) {
 	}
 }
 
+// NewJob sends a new job to the next running in the queue.
 func (r *RunnerHTTPApiV1) NewJob(cJob controller.Job) {
 	wr, err := r.wrQueue.Pop()
 	if err != nil {
@@ -78,14 +83,17 @@ func (r *RunnerHTTPApiV1) NewJob(cJob controller.Job) {
 	wr.CallbackChan <- cJob
 }
 
+// NeedNewJob returns a boolean indicating whether or not there are waiting runners.
 func (r *RunnerHTTPApiV1) NeedNewJob() bool {
 	return !r.wrQueue.Empty()
 }
 
+// NullifyUUIDs marks a job uuid as null.
 func (r *RunnerHTTPApiV1) NullifyUUIDs(uuids []controller.UUID) {
 	r.nullifiedUUIDs = append(r.nullifiedUUIDs, uuids...)
 }
 
+// WaitingRunners returns a list of Runner names which are waiting for jobs.
 func (r *RunnerHTTPApiV1) WaitingRunners() (runnerNames []string) {
 	wr := r.wrQueue.Dequeue()
 
@@ -96,18 +104,19 @@ func (r *RunnerHTTPApiV1) WaitingRunners() (runnerNames []string) {
 	return
 }
 
-func (a *RunnerHTTPApiV1) requestJob(w http.ResponseWriter, r *http.Request) {
-	a.logger.Trace("%+v", r)
-	switch r.Method {
+// requestJob is an HTTP handler which handles Runner job requests.
+func (r *RunnerHTTPApiV1) requestJob(w http.ResponseWriter, hr *http.Request) {
+	r.logger.Trace("%+v", hr)
+	switch hr.Method {
 	case http.MethodGet:
 		// Gather Runner name from HTTP headers
-		runnerName := r.Header.Get("X-Encodarr-Runner-Name")
-		a.logger.Info("Received request from %v @ %v", runnerName, r.RemoteAddr)
+		runnerName := hr.Header.Get("X-Encodarr-Runner-Name")
+		r.logger.Info("Received request from %v @ %v", runnerName, hr.RemoteAddr)
 
 		// Add callback channel to waiting runners queue
 		receiveChan := make(chan controller.Job)
 		requestUUID := uuid.NewString()
-		a.wrQueue.Push(waitingRunner{Name: runnerName, CallbackChan: receiveChan, UUID: requestUUID})
+		r.wrQueue.Push(waitingRunner{Name: runnerName, CallbackChan: receiveChan, UUID: requestUUID})
 
 		// Check for a returned job
 		var jobToSend controller.Job
@@ -115,8 +124,8 @@ func (a *RunnerHTTPApiV1) requestJob(w http.ResponseWriter, r *http.Request) {
 		select {
 		case jobToSend, ok = <-receiveChan:
 			break
-		case <-r.Context().Done():
-			a.wrQueue.Remove(requestUUID)
+		case <-hr.Context().Done():
+			r.wrQueue.Remove(requestUUID)
 			w.WriteHeader(http.StatusGone)
 			return
 		}
@@ -131,7 +140,7 @@ func (a *RunnerHTTPApiV1) requestJob(w http.ResponseWriter, r *http.Request) {
 		// Marshal Job into json to be sent in a header
 		jobJSONBytes, err := json.Marshal(jobToSend)
 		if err != nil {
-			a.logger.Error("error marshaling Job to json: %v", err)
+			r.logger.Error("error marshaling Job to json: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
@@ -141,7 +150,7 @@ func (a *RunnerHTTPApiV1) requestJob(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", inferMIMETypeFromExt(filepath.Ext(jobToSend.Path)))
 		file, err := os.Open(jobToSend.Path)
 		if err != nil {
-			a.logger.Error("error opening %v: %v", jobToSend.Path, err)
+			r.logger.Error("error opening %v: %v", jobToSend.Path, err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
@@ -154,7 +163,7 @@ func (a *RunnerHTTPApiV1) requestJob(w http.ResponseWriter, r *http.Request) {
 			bytesRead, err := file.Read(buffer)
 			if err != nil {
 				if err != io.EOF {
-					a.logger.Error("error writing to HTTP Writer: %v", err)
+					r.logger.Error("error writing to HTTP Writer: %v", err)
 					w.WriteHeader(http.StatusInternalServerError)
 					return
 				}
@@ -167,26 +176,27 @@ func (a *RunnerHTTPApiV1) requestJob(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (a *RunnerHTTPApiV1) jobStatus(w http.ResponseWriter, r *http.Request) {
-	a.logger.Trace("%+v", r)
-	switch r.Method {
+// jobStatus in an HTTP handler for status updates from Runners.
+func (r *RunnerHTTPApiV1) jobStatus(w http.ResponseWriter, hr *http.Request) {
+	r.logger.Trace("%+v", hr)
+	switch hr.Method {
 	case http.MethodPost:
-		b, err := io.ReadAll(r.Body)
+		b, err := io.ReadAll(hr.Body)
 		if err != nil {
-			a.logger.Error("error reading job status body: %v", err)
+			r.logger.Error("error reading job status body: %v", err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
 		ijs := incomingJobStatus{}
 		if err = json.Unmarshal(b, &ijs); err != nil {
-			a.logger.Error("error unmarshalling into struct: %v", err)
+			r.logger.Error("error unmarshalling into struct: %v", err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
 		// Check ijs.UUID against nullified UUIDs
-		for _, v := range a.nullifiedUUIDs {
+		for _, v := range r.nullifiedUUIDs {
 			if v == ijs.UUID {
 				w.WriteHeader(http.StatusConflict) // Send the 409 error code to signal to the Runner to indicate that the job has been nullified.
 				return
@@ -194,9 +204,9 @@ func (a *RunnerHTTPApiV1) jobStatus(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// Get existing DispatchedJob from datastore
-		dJob, err := a.ds.DispatchedJob(ijs.UUID)
+		dJob, err := r.ds.DispatchedJob(ijs.UUID)
 		if err != nil {
-			a.logger.Error(err.Error())
+			r.logger.Error(err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
@@ -207,8 +217,8 @@ func (a *RunnerHTTPApiV1) jobStatus(w http.ResponseWriter, r *http.Request) {
 		dJob.LastUpdated = time.Now()
 
 		// Store DispatchedJob into datastore
-		if err = a.ds.SaveDispatchedJob(dJob); err != nil {
-			a.logger.Error(err.Error())
+		if err = r.ds.SaveDispatchedJob(dJob); err != nil {
+			r.logger.Error(err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
@@ -219,16 +229,17 @@ func (a *RunnerHTTPApiV1) jobStatus(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (a *RunnerHTTPApiV1) jobComplete(w http.ResponseWriter, r *http.Request) {
-	a.logger.Trace("%+v", r)
-	switch r.Method {
+// jobComplete is an HTTP handler for Runners to upload completed jobs to.
+func (r *RunnerHTTPApiV1) jobComplete(w http.ResponseWriter, hr *http.Request) {
+	r.logger.Trace("%+v", hr)
+	switch hr.Method {
 	case http.MethodPost:
 		//? Probably should detect a client disconnect and disregard any data it sent (use r.Context())
 
 		// Read history entry from headers
-		h := r.Header.Get("X-Encodarr-History-Entry")
+		h := hr.Header.Get("X-Encodarr-History-Entry")
 		if h == "" {
-			a.logger.Debug("received invalid history entry")
+			r.logger.Debug("received invalid history entry")
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
@@ -237,13 +248,13 @@ func (a *RunnerHTTPApiV1) jobComplete(w http.ResponseWriter, r *http.Request) {
 		cJob := controller.CompletedJob{}
 		err := json.Unmarshal([]byte(h), &cJob)
 		if err != nil {
-			a.logger.Debug("error unmarshalling history entry: %v", err)
+			r.logger.Debug("error unmarshalling history entry: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
 		// If UUID was nullified, respond with 409 error code and exit
-		for _, v := range a.nullifiedUUIDs {
+		for _, v := range r.nullifiedUUIDs {
 			if v == cJob.UUID {
 				w.WriteHeader(http.StatusConflict)
 				return
@@ -252,9 +263,9 @@ func (a *RunnerHTTPApiV1) jobComplete(w http.ResponseWriter, r *http.Request) {
 
 		// If job didn't fail, write file to disk
 		if !cJob.Failed {
-			fileReader, fileHeader, err := r.FormFile("file")
+			fileReader, fileHeader, err := hr.FormFile("file")
 			if err != nil {
-				a.logger.Debug("error accessing form file: %v", err)
+				r.logger.Debug("error accessing form file: %v", err)
 				w.WriteHeader(http.StatusBadRequest)
 				return
 			}
@@ -264,7 +275,7 @@ func (a *RunnerHTTPApiV1) jobComplete(w http.ResponseWriter, r *http.Request) {
 			cJob.InFile = fmt.Sprintf("%v.import%v", cJob.UUID, filepath.Ext(fileHeader.Filename))
 			f, err := os.Create(cJob.InFile) // TODO: Mock out for testing
 			if err != nil {
-				a.logger.Debug("error opening receiving file: %v", err)
+				r.logger.Debug("error opening receiving file: %v", err)
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
@@ -273,7 +284,7 @@ func (a *RunnerHTTPApiV1) jobComplete(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// Add controller.CompletedJob to channel for CompletedJobs to pick up from
-		a.completedJobs <- cJob
+		r.completedJobs <- cJob
 
 		w.WriteHeader(http.StatusOK)
 	default:
@@ -281,6 +292,7 @@ func (a *RunnerHTTPApiV1) jobComplete(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// incomingJobStatus defines the structure of the job status from Runners.
 type incomingJobStatus struct {
 	UUID   controller.UUID      `json:"uuid"`
 	Status controller.JobStatus `json:"status"`
