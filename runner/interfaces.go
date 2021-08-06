c8b17c9d502182cc373f93eeb10eec702461e892
Brenek Harrison <brenekharrison@gmail.com>
2021-08-06 16:32:42 -0600

    Fix golint issues in runner/

--- runner/interfaces.go
+++ runner/interfaces.go
@@ -2,12 +2,14 @@ package runner
 
 import "context"
 
+// Communicator defines how a struct which talks with a Controller should behave.
 type Communicator interface {
 	SendJobComplete(*context.Context, JobInfo, CommandResults) error
 	SendNewJobRequest(*context.Context) (JobInfo, error)
 	SendStatus(*context.Context, string, JobStatus) error
 }
 
+// CommandRunner defines how a struct which runs the FFmpeg commands should behave.
 type CommandRunner interface {
 	Done() bool
 	Start(JobInfo)
--- runner/run.go
+++ runner/run.go
@@ -106,7 +106,7 @@ func Run(ctx *context.Context, c Communicator, r CommandRunner, testMode bool) {
 	}
 }
 
-// IsContextFinshed returns a boolean indicating whether or not a context.Context is finished.
+// IsContextFinished returns a boolean indicating whether or not a context.Context is finished.
 // This replaces the need to use a select code block.
 func IsContextFinished(ctx *context.Context) bool {
 	select {
