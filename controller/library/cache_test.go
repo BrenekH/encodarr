a07a0ec573e71e1b82dca4d581420eb0ad9788b7
Brenek Harrison <brenekharrison@gmail.com>
2021-09-23 11:53:49 -0600

    Define more tests in comments

---
+++ controller/library/cache_test.go
@@ -0,0 +1,5 @@
+package library
+
+// TODO: Test Cache
+//   - NewCache actually sets the provided structs to the inner fields
+//   = Read - Reading from metadataReader when the modtimes are different. Returning stored metadata when modtimes are same.
